2a6182898c6b51e314c3ec7f5a14dcc5b1cede75
十隻餒 <alice258699@gmail.com>
2026-09-24 02:19:23 +0800

    總之請他幫我

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/font-collection',
+  assetPrefix: '/font-collection/',
   images: {
     unoptimized: true
   }
